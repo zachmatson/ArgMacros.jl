e9685940a74c93b27ca4b15257f6eb3eea98cc85
zachmatson <zacharywdmatson@gmail.com>
2020-05-18 00:20:52 -0400

    Set up Documenter build

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,6 @@ makedocs(
     modules = [ArgMacros]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/zachmatson/ArgMacros.jl.git",
+)
